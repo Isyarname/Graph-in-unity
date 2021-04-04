e8f2d578612cb2cfa5d04e3a527e448e67dd8fbd
Isyarname <alalala902@gmail.com>
2021-04-04 12:21:50 +0400

    initial commit

---
+++ Assets/Graphs/Scripts/Graph.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Node startNode;
+    public Node finisgNode;
+    public List<Node> graph;
+    [Range(0,5)]public float delay;
+
+    public bool IsDesiredNode(Node node)
+    {
+    	if (node.isFinishNode) return true;
+    	return false;
+    }
+
+    public IEnumerator BreadthFirstSearch()
+    {
+    	//создаём очередь с соседями стартовой точки
+    	var searchQueue = new Queue<Node>(startNode.neighbors);
+    	//создаём список для уже проверенных узлов
+    	var checkedNodes = new List<Node>();
+    	//пока очередь не ппуста извлека
+    	while (searchQueue.Count > 0)
+    	{
+    		var node = searchQueue.Dequeue();
+    		if (!HasNodeInList(node, checkedNodes))
+    		{
+    			if (IsDesiredNode(node))
+    			{
+    				Debug.Log("finish node is" + node.name);
+    				
+    			}
+    			else
+    			{
+    				Debug.Log("ааааааааааааааааааа");
+    				foreach (var n in node.neighbors)
+    				{
+    					searchQueue.Enqueue(n);
+    				}
+    				checkedNodes.Add(node);
+    				node.isChecked = true;
+    			}
+    		}
+    		yield return new WaitForSeconds(delay);
+    	}
+    }
+
+    private bool HasNodeInList(Node node, List<Node> nodesList)
+    {
+    	foreach (var n in nodesList)
+    	{
+    		if (n == node) return true;
+    	}
+    	return false;
+    }
+
+    private void SetGraph()
+    {
+    	foreach (Transform t in transform)
+		{
+			graph.Add(t.GetComponent<Node>());
+		}
+    }
+
+	private void OnDrawGizmos()
+	{
+		if (graph.Count == 0)
+		{
+			SetGraph();
+			StartCoroutine(BreadthFirstSearch());
+		}
+	}
+
+	private void Start()
+	{
+		StartCoroutine(BreadthFirstSearch());
+	}
+}
---
+++ Assets/Graphs/Scripts/Node.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public List<Node> neighbors;
+    public List<float> distanceToNeighbors;
+
+    public bool isStartNode;
+    public bool isFinishNode;
+    public bool isChecked;
+
+    private void OnDrawGizmos()
+    {
+    	Gizmos.color = Color.white;
+    	if (isStartNode) Gizmos.color = Color.blue;
+    	if (isFinishNode) Gizmos.color = Color.red;
+    	if (isChecked) Gizmos.color = Color.yellow;
+    	Gizmos.DrawCube(transform.position, Vector3.one);
+    	if (neighbors.Count > 0){
+    		distanceToNeighbors.Clear();
+    		foreach (var node in neighbors)
+    		{
+    			if (node != null)
+    			{
+		    		Debug.DrawLine(transform.position, node.transform.position, Color.green);
+		    		distanceToNeighbors.Add(
+		    			Vector3.Distance(transform.position, node.transform.position)
+		    		);
+		    	}
+    		}
+    	}
+    	
+    }
+}
