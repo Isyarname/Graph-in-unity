2c0772c9f1f95ae02818c5b04f681aeb1710d462
Isyarname <alalala902@gmail.com>
2021-04-18 17:51:13 +0400

    потеплело

--- Assets/Graphs/Scripts/Graph.cs
+++ Assets/Graphs/Scripts/Graph.cs
@@ -1,18 +1,93 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
     public Node startNode;
-    public Node finisgNode;
+    public Node finishNode;
     public List<Node> graph;
+    [SerializeField] private GameObject nodePrefab;
     [Range(0,5)]public float delay;
+    public Node[,] matrix = new Node[10,10];
+    private List<Node> checkedNodes = new List<Node>();
+    private List<Node> parents = new List<Node>();
 
     public bool IsDesiredNode(Node node)
     {
     	if (node.isFinishNode) return true;
     	return false;
+    }
+
+    public void GenerateGraph()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            for (var j = 0; j < 10; j++)
+            {
+                var position = new Vector3(i, 0, j);
+                var node = Instantiate(nodePrefab, position, Quaternion.identity);
+                var n = node.GetComponent<Node>();
+                if (i == 0 && j == 0)
+                {
+                    n.isStartNode = true;
+                    startNode = n;
+                }
+                else if (i == 9 && j == 9) {
+                    n.isFinishNode = true;
+                    finishNode = n;
+                }
+                node.transform.SetParent(transform);
+                matrix[i, j] = n;
+            }
+        }
+        CreateLinks();
+    }
+
+    private void CreateLinks()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                try 
+                {
+                   matrix[i,j].neighbors.Add(matrix[i+1,j]); 
+                } 
+                catch (IndexOutOfRangeException) 
+                {
+                    Debug.Log("Fail");
+                }
+                try 
+                {
+                   matrix[i,j].neighbors.Add(matrix[i,j+1]); 
+                } 
+                catch (IndexOutOfRangeException) 
+                {
+                    Debug.Log("Fail");
+                }
+                try 
+                {
+                   matrix[i,j].neighbors.Add(matrix[i-1,j]);
+                } 
+                catch (IndexOutOfRangeException) 
+                {
+                    Debug.Log("Fail");
+                }
+                try 
+                {
+                   matrix[i,j].neighbors.Add(matrix[i,j-1]); 
+                } 
+                catch (IndexOutOfRangeException) 
+                {
+                    Debug.Log("Fail");
+                }
+                
+                
+            }
+        }
+        //StartCoroutine(BreadthFirstSearch());
     }
 
     public IEnumerator BreadthFirstSearch()
@@ -46,6 +121,59 @@ public class Graph : MonoBehaviour
     		yield return new WaitForSeconds(delay);
     	}
     }
+/*
+    public IEnumerator Dijkstras()
+    {
+        Node node = startNode;
+
+
+        while(!node.isFinishNode)
+        {
+            Debug.Log(node.name);
+            float cost = node.label;
+            
+            List<Node> neighbors = node.neighbors;
+            
+            for (var i = 0; i < neighbors.Count; i++) 
+            {
+                //Debug.Log(cost);
+                //Debug.Log(node.distanceToNeighbors[i]);
+                var newCost = cost + node.distanceToNeighbors[i];
+                // новая стоимость для соседних точек
+                print(neighbors[i].label > newCost);//newCost+""+neighbors[i].label);
+                if (neighbors[i].label > newCost)
+                // если новая стоимость меньше старой для соседних точек
+                {
+                    neighbors[i].isChecked = true;
+                    neighbors[i].unknownLabel = false;
+                    neighbors[i].label = newCost;
+                    // ярлык - новая стоимость
+                    if (node != null) neighbors[i].parent = node;
+                    // обновление родителя с наименьшей стоимостью
+                }
+            }
+            checkedNodes.Add(node);
+            node = FindLowestCostNode();
+            //Debug.Log(node.name);
+            yield return new WaitForSeconds(4);
+        }
+    }
+
+    private Node FindLowestCostNode()
+    {
+        float lowestCost = float.PositiveInfinity;
+        Node lowestCostNode = null;
+        for (var i = 0; i < graph.Count; i++) 
+        {
+            if (graph[i].label < lowestCost && !HasNodeInList(graph[i], checkedNodes))
+            {
+                Debug.Log(graph[i].name + "checked");
+                lowestCost = graph[i].label;
+                lowestCostNode = graph[i];
+            }
+        }
+        return lowestCostNode;
+    }
 
     private bool HasNodeInList(Node node, List<Node> nodesList)
     {
@@ -55,6 +183,62 @@ public class Graph : MonoBehaviour
     	}
     	return false;
     }
+*/
+    private bool HasNodeInList(Node node, List<Node> nodesList)
+    {
+        foreach (var n in nodesList)
+        {
+            if (n == node) return true;
+        }
+        return false;
+    }
+    
+    public IEnumerator Dijkstras()
+    {
+        Node node = startNode;
+
+        while(!node.isFinishNode)
+        {
+            Debug.Log(node.name);
+            float cost = node.label;
+            List<Node> neighbors = node.neighbors;
+            for (var i = 0; i < neighbors.Count; i++) 
+            {
+                Debug.Log(cost);
+                Debug.Log(node.distanceToNeighbors[i]);
+                var newCost = cost + node.distanceToNeighbors[i];
+                neighbors[i].isChecked = true; 
+                if (neighbors[i].label > newCost)
+                {
+                    neighbors[i].unknownLabel = false; 
+                    neighbors[i].label = newCost; 
+                    if (node != null) neighbors[i].parent = node;
+                }
+            } 
+            checkedNodes.Add(node); 
+            node = FindLowestCostNode();
+            Debug.Log(node.name);
+            yield return new WaitForSeconds(4);
+        }
+    }
+
+    private Node FindLowestCostNode()
+    {
+        float lowestCost = float.PositiveInfinity;
+        Node lowestCostNode = null;
+        for (var i = 0; i < graph.Count; i++)
+        {
+            if (graph[i].label < lowestCost && !HasNodeInList(graph[i], checkedNodes))
+            {
+                Debug.Log(graph[i].name + " checked");
+                lowestCost = graph[i].label;
+                lowestCostNode = graph[i];
+            }
+        }
+        return lowestCostNode;
+    }
+
+
 
     private void SetGraph()
     {
@@ -69,12 +253,14 @@ public class Graph : MonoBehaviour
 		if (graph.Count == 0)
 		{
 			SetGraph();
-			StartCoroutine(BreadthFirstSearch());
+			//StartCoroutine(BreadthFirstSearch());
 		}
 	}
 
 	private void Start()
 	{
-		StartCoroutine(BreadthFirstSearch());
+        StartCoroutine(Dijkstras());
+        //GenerateGraph();
+		//StartCoroutine(BreadthFirstSearch());
 	}
 }
--- Assets/Graphs/Scripts/Node.cs
+++ Assets/Graphs/Scripts/Node.cs
@@ -10,27 +10,37 @@ public class Node : MonoBehaviour
     public bool isStartNode;
     public bool isFinishNode;
     public bool isChecked;
+    public bool unknownLabel = true;
+    public float label;
+    public Node parent;
 
     private void OnDrawGizmos()
     {
+        if (unknownLabel) label = float.PositiveInfinity;
     	Gizmos.color = Color.white;
     	if (isStartNode) Gizmos.color = Color.blue;
     	if (isFinishNode) Gizmos.color = Color.red;
     	if (isChecked) Gizmos.color = Color.yellow;
     	Gizmos.DrawCube(transform.position, Vector3.one);
-    	if (neighbors.Count > 0){
-    		distanceToNeighbors.Clear();
-    		foreach (var node in neighbors)
-    		{
-    			if (node != null)
-    			{
-		    		Debug.DrawLine(transform.position, node.transform.position, Color.green);
-		    		distanceToNeighbors.Add(
-		    			Vector3.Distance(transform.position, node.transform.position)
-		    		);
-		    	}
-    		}
-    	}
-    	
+        
+        if (neighbors.Count > 0)
+        {
+            distanceToNeighbors.Clear();
+            foreach (var node in neighbors)
+            {
+                if (node != null)
+                {
+                    Debug.DrawLine(transform.position, node.transform.position, Color.green);
+                    distanceToNeighbors.Add(
+                        Vector3.Distance(transform.position, node.transform.position)
+                    );
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        
     }
 }
